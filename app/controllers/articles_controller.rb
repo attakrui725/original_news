9a733ec39b0012d587f5aa7603672d248cbb6923
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2020-12-10 18:52:09 +0900

    投稿機能の実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,26 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 
   def new
-    @articlse = Articles.new
+    @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      binding.pry
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+private
+def article_params
+  params.require(:article).permit(:name, :description, :image).merge(user_id: current_user.id)
+  
+end
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,12 @@
 class Article < ApplicationRecord
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :user_id
+  end
+
   
+belongs_to :user
+has_one_attached :image
 end
