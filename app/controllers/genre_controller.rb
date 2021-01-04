f3e49ee3b7bea9941884e5b7cc7c78e9130dd8dc
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2021-01-04 10:58:53 +0900

    ジャンル別表示

---
+++ app/controllers/genre_controller.rb
@@ -0,0 +1,5 @@
+class GenreController < ApplicationController
+  def show
+    @article = Article.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'genre/show'
   devise_for :users
   root to: 'articles#index'
   resources :articles, only: [:new, :index, :create, :show,:update, :destroy] do
   resources :messages, only: [:create, :new]
   end
+  resources :genres, only: :show
 end
