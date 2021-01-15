6428d3aba3be8711ee9c882053c17c7bd0d6a138
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2021-01-15 14:03:37 +0900

    トップページの編集

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,11 @@
 class FavoritesController < ApplicationController
-
   def create
     @article = Article.find(params[:id])
     Favorite.create(user_id: current_user.id, article_id: params[:id])
-
   end
 
   def destroy
     @article = Article.find(params[:id])
     Favorite.find_by(user_id: current_user.id, article_id: params[:id]).destroy
-
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable
-validates :name, presence: true
+         :recoverable, :rememberable, :validatable
+  validates :name, presence: true
 
-has_many :articles
-has_many :messages
-has_many :favorites, dependent: :destroy
-
+  has_many :articles
+  has_many :messages
+  has_many :favorites, dependent: :destroy
 
   def liked_by?(article_id)
-      favorites.where(article_id: article_id).exists?
+    favorites.where(article_id: article_id).exists?
   end
 end
