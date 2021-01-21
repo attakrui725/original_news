702a3a2cf99432e539d84a7b5e5dd82c6cd82888
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2021-01-21 11:33:01 +0900

    マイページにフォローボタンの設置

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'genre/show'
   devise_for :users
   root to: 'articles#index'
   resources :articles, only: [:new, :index, :create, :show,:update, :destroy] do
   resources :messages, only: [:create, :new]
   end
+  resources :relationships, only: [:create, :destroy]
+
   post 'favorite/:id' => 'favorites#create', as: 'create_favorite'
   delete 'favorite/:id' => 'favorites#destroy', as:'destroy_favorite'
   resources :genres, only: :show
