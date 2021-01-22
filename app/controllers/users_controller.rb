d2b9a83a1c029737d2bcc16890ed8b3b3153413d
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2021-01-22 19:21:56 +0900

    フォローフォロワーに対するフィルタリング機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @relationship = current_user.relationships.find_by(follow_id: @user.id)
     @set_relationship = current_user.relationships.new
-    @users = @user.followings.all
+
 
   end
 
