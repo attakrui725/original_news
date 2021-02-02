bc5c2e5e8c41a664194cd619064bb6bb353c731f
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2021-02-02 17:51:27 +0900

    エラー修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,7 +50,7 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
-      user.neme = "take"
+      user.name = "test"
     end
   end
 end
