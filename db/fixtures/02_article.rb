f6a81e0d5a398fc0d21bf89493ea4475ca191fec
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2021-02-02 21:46:18 +0900

    テストデータ修正

--- db/fixtures/02_article.rb
+++ db/fixtures/02_article.rb
@@ -4,8 +4,8 @@ s.name = '[ドコモの新プラン“プレミア”と「ahamo」の比較・�
 s.description ='NTTドコモは12月18日、かねてより予告していた大容量プランの料金を見直した新料金プラン「5Gギガホ プレミア」「ギガホ プレミア」を発表した。'
 s.url = 'https://k-tai.watch.impress.co.jp/docs/review/1296635.html'
 s.img = 'https://k-tai.watch.impress.co.jp/img/ktw/list/1296/635/cont009.jpg'
-s.user_id = '1'
-s.genre_id = '1'
+s.user_id = 1
+s.genre_id = 1
 end
 
 Article.seed do |s|
@@ -14,8 +14,8 @@ s.name = 'ソフトバンク、20GB／2980円の新料金プランを2021年3月
 s.description ='ソフトバンクは、2021年3月以降にデータ容量20GBで月額2980円（税抜、以下同）の新料金プランを提供する。22日に開催された、新料金発表会で発表された。'
 s.url = 'https://k-tai.watch.impress.co.jp/docs/news/1296717.html'
 s.img = 'https://k-tai.watch.impress.co.jp/img/ktw/list/1296/717/cont1.JPG'
-s.user_id = '2'
-s.genre_id = '2'
+s.user_id = 2
+s.genre_id = 2
 end
 
 Article.seed do |s|
@@ -24,8 +24,8 @@ s.name = '【2021年1月】ふるさと納税 還元率ランキングベスト1
 s.description ='【1月20日更新】最大還元率109％！12のふるさと納税サイトに掲載されている全返礼品の高還元率ランキング。家電や金券、米や肉など合計26のカテゴリ別コスパランキングもご用意。一般消費者が購入できる金額で考えた時の「還元率」が3割を上回るお得な返礼品は、まだ存在します。各自治体は「返礼品の調達額（返礼率）を寄付金額の3割以下とすること」が義務付けられていますが、市場に出回っている販売価格よりも安く調達できているケースが多々あるからです。'
 s.url = 'https://furu-sato.com/total/value_rank'
 s.img = 'https://furu-sato.com/assets/rank/ogp-total-value-rank-109-fa38f5865b1b4706c89a10e3d679d84a078767926549875382b9048199cc596b.png'
-s.user_id = '4'
-s.genre_id = '4'
+s.user_id = 4
+s.genre_id = 4
 end
 
 Article.seed do |s|
@@ -34,6 +34,6 @@ s.name = '中部電力ミライズ、口コミ・評判は？メリットやデ�
 s.description ='中部電力ミライズの電気料金プランに実際に切り替えた方から、電気料金・サポート体制・会員サービスなどについて口コミ・評判を聞き、メリットとデメリットをまとめました。さらに切り替え先を悩んでいる方に向けて、おすすめの電力会社も紹介します。'
 s.url = 'https://enechange.jp/articles/chuden-review'
 s.img = 'https://enechange.jp/articles/wp-content/uploads/2020/08/1a0b6c7b882aaaffaf0a3eb90034e9bd.jpg'
-s.user_id = '3'
-s.genre_id = '3'
+s.user_id = 3
+s.genre_id = 3
 end
