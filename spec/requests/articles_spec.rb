5b7d117455cdefd918d7d94819e45a904f1f3ec6
attakrui725 <73804663+attakrui725@users.noreply.github.com>
2021-02-10 23:29:46 +0900

    artilcleコントローラーのテストコードの追加

---
+++ spec/requests/articles_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+RSpec.describe ArticlesController, type: :request do
+let(:article) { build(:article) }
+
+  describe "GET /index" do
+    it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do
+      get root_path
+    binding.pry
+    end
+    it 'indexアクションにリクエストするとレスポンスに投稿済みの記事のタイトルが存在する' do
+    end
+    it 'indexアクションにリクエストするとレスポンスに投稿済みの記事のの詳細が存在する' do
+    end
+    it 'indexアクションにリクエストするとレスポンスに投稿済みの記事の写真が存在する' do
+    end
+  end
+end
